1c49984290a92e9ff9b3e31c153017af0a2f51ea
Dillan May <dillanmay_6@yahoo.com>
2022-12-06 14:10:15 -0600

    finished Navbar compenent

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar, Register, Activities, Routines } from "./";
 
@@ -10,7 +10,7 @@ const Main = () => {
 
   return (
     <div id="main">
-      <BrowserRouter>
+      
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Login } from "./";
+import { NavLink } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = (props) => {
   return (
     <div id="navbar">
-      <h2> I am navbar</h2>
+      <NavLink id="navLogin" to="Login">
+        Login
+      </NavLink>
+      <NavLink id="navRoutines" to="Routines">
+        Routines
+      </NavLink>
+      <NavLink id="navActivities" to="Activities">
+        Activities
+      </NavLink>
+      {props.isLogin ? (
+        <button>
+          id="logOut" onClick=
+          {() => {
+            props.SetLogin(false);
+            localStorage.removeItem("token");
+          }}
+          Log Out
+        </button>
+      ) : null}
     </div>
   );
 };
