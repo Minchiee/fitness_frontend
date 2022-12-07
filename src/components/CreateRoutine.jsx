3cf12b39ea60bb2bab909b0375ae5f34b9b17415
Minh Nguyen <lambandwoof@gmail.com>
2022-12-06 21:53:32 -0600

    finished public routines and public activities

--- src/components/CreateRoutine.jsx
+++ src/components/CreateRoutine.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { addRoutine } from '../api/index'
 
-const CreateRoutine = () => {
+const CreateRoutine = (props) => {
+    const [name, setName] = useState('');
+    const [goal, setGoal] = useState('');
+    const [isPublic, setIsPublic] = useState('');
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const newActivity = await addRoutine({
+            name: name,
+            goal: goal,
+            isPublic: isPublic
+        })
+    }
+    
+    return (
     <div id="CreateRoutine"></div>
+    )
+    
 }
 
 export default CreateRoutine;
--- src/components/Routines.jsx
+++ src/components/Routines.jsx
@@ -1,7 +1,31 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import { getRoutines } from '../api';
 
 const Routines = () => {
-    <div id="routines"></div>
-}
+  const [routines, setRoutines] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const routinesData = await getRoutines();
+      setRoutines(routinesData);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>All Public Routines List</h1>
+      <div>
+        {routines.map((routine) => (
+          <div key={routine.id}>
+            <p>Creator Name: {routine.creatorName}</p>
+            <p>CreatorId: {routine.creatorId}</p>
+            <p>Goal: {routine.goal}</p>
+            <p>------------------------</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 export default Routines;
