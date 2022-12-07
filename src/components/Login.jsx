747aea0103f2394f43219d8d5ef531bbbebbf25f
Dillan May <dillanmay_6@yahoo.com>
2022-12-06 19:52:58 -0600

    created login function

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { loginUser } from "../api";
 
-const Login = () => {
-    <div id="login"></div>
-}
+const Login = (props) => {
+    const [username, setUsername] = useState([]);
+    let navigate = useNavigate();
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const username = event.target[0].value;
+        const password = event.targer[1].value;
+        const loggedInUser = await loginUser(username, password);
+        const token = loggedInUser.token;
+        props.setLogin(true);
+        localStorage.removeItem("token");
+        localStorage.setItem("token", token);
+        localStorage.removeItem("username");
+        localStorage.setItem("username", username);
+        setUsername(username);
+        props.userLogin();
+        navigate("/MyRoutines")
+    }
+    return(
+        <div>
+            <div id="logIn">Log In</div>
+            <div className="loginForm">
+                <form onSubmit={handleSubmit}>
+                    <label id="user" htmlFor="password">
+                        Username:{" "}
+                    </label>
+                    <input id="username" type="text" required />
+                    <label id="pass" htmlFor="password">
+                        Password:{" "}
+                    </label>
+                    <input id="password" type="password" />
+                    <button id="loginBtn" type="submit">
+                        Log In
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+};
 
 export default Login;
